2e46cfaabfff711da45af30640f2983e5da7afe9
Dexter Haslem <dexter@gitprime.com>
2018-08-04 18:07:01 -0600

    building out api call
    getting last x hours is tricky cuz historical requests only go for day
    requested cut off at midnight so its not trivial to 'get last 12 hours',
    might just request last 2 days unconditionally

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from furl import furl
+from requests import get
+from time import time
+
 APIKEYFILE = 'darksky.key'
 
 
@@ -7,8 +11,38 @@ def get_api_key():
         return l.strip()
 
 
+def build_api_url(**kwargs):
+    url = furl('https://api.darksky.net')
+    url.path.add('forecast')
+    url.path.add(kwargs.get('key'))
+
+    lat = str(kwargs.get('lat'))
+    long = str(kwargs.get('long'))
+
+    # [lat,long,time]
+    try_time = kwargs.get('time')
+    args = [lat, long]
+    if try_time:
+        unix_time = str(try_time)
+        args.append(unix_time)
+
+    darksky_args = ','.join(args)
+    url.path.add(darksky_args)
+    # url.args.add('exclude', 'currently,flags,hourly')
+    return url.tostr()
+
+
 def main():
-    print get_api_key()
+    key = get_api_key()
+    lat = 39.559319
+    long = -105.099653
+    unix_time = int(time())
+
+    url = build_api_url(key=key, lat=lat, long=long)  # , time=unix_time)
+    req = get(url)
+    resp = req.json()
+
+    print resp
 
 
 if __name__ == "__main__":
